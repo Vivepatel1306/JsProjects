354662843926d0100c7148cc1469fca6d1b9c5a1
AlphaRange001 <vp181431@gmail.com>
2024-12-10 03:30:16 +0530

    TicTacToe concept Ready

---
+++ TicTacToe/concept.js
@@ -0,0 +1,102 @@
+// let arr=[[1,0,],[1,0,1],[1,0,]]
+// // let a=1;
+// let firsti=0,seci=0,thirdi=0;
+// let firstj=0,secj=0,thirdj=0;
+// let firsti1=0,seci1=0,thirdi1=0;
+// let firstj1=0,secj1=0,thirdj1=0;
+// for(let i=0;i<3;i++){
+//     for(let j=0;j<3;j++){
+//     if(arr[i][j]==1){
+//         // a!=a
+//         switch(i){
+//             case 0: firsti++
+//             if(firsti>2){
+//                 console.log("user 1 won");
+//             }
+//             break;
+
+//             case 1: seci++
+//             if(seci>2){
+//                 console.log("user 1 won");
+//             }
+//             break;
+
+//             case 2: thirdi++
+//             if(thirdi>2){           
+//                 console.log("after:"+thirdi);
+//                 console.log("user 13 won");
+//             }
+//             break;
+
+//         }
+//         switch(j){
+//             case 0: firstj++
+//             if(firstj>2){
+//                 console.log("user 14 won");
+//             }
+//             break;
+
+//             case 1: secj++
+//             if(secj>2){
+//                 console.log("user 15 won");
+//             }
+//             break;
+
+//             case 2: thirdj++
+//             if(thirdj>2){
+//                 console.log("user 16 won");
+//             }
+//             break;
+
+//         }
+//     }
+//     if(arr[i][j]==0){
+//         // a=!a
+//         switch(i){
+//             case 0: firsti1++
+//             if(firsti1>2){
+//                 console.log("user 2 won");
+//             }
+//             break;
+
+//             case 1: seci1++
+//             if(seci1>2){
+//                 console.log("user 2 won");
+//             }
+//             break;
+
+//             case 2: thirdi1++
+//             if(thirdi1>2){
+//                 console.log("user 2 won");
+//             }
+//             break;
+
+//         }
+//         switch(j){
+//             case 0: firstj1++
+//             if(firstj1>2){
+//                 console.log("user 2 won");
+//             }
+//             break;
+
+//             case 1: secj1++
+//             if(secj1>2){
+//                 console.log("user 2 won");
+//             }
+//             break;
+
+//             case 2: thirdj1++
+//             if(thirdj1>2){
+//                 console.log("user 2 won");
+//             }
+//             break;
+
+//         }
+//     }
+// }
+// }
+
+
+
+let row=document.querySelector("span")
+let column=document.querySelectorAll("input")
