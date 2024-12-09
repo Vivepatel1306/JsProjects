3e0a1aaf20a9054a083057c00c986cb0bb94f88d
AlphaRange001 <vp181431@gmail.com>
2024-12-09 05:58:51 +0530

    Passwrd Strength Indicator

--- HorizntalScrolling/concept.js
+++ HorizntalScrolling/concept.js
@@ -1,10 +1,56 @@
-let next = document.querySelectorAll("button")[1]
-next.addEventListener('click', function () {
-    let NextImg = document.querySelector(".second")
-    NextImg.style.transform = "translatex(-83.5vh)"
-})
-let back = document.querySelectorAll("button")[0]
-back.addEventListener('click', function () {
-    let NextImg = document.querySelector(".second")
-    NextImg.style.transform = "translatex(0vh)"
-})
+// let next = document.querySelectorAll("button")[1]
+// next.addEventListener('click', function () {
+//     let NextImg = document.querySelector(".second")
+//     NextImg.style.transform = "translatex(-83.5vh)"
+// })
+// let back = document.querySelectorAll("button")[0]
+// back.addEventListener('click', function () {
+//     let NextImg = document.querySelector(".second")
+//     NextImg.style.transform = "translatex(0vh)"
+// })
+
+
+let image = document.querySelectorAll("img")
+let a = 0
+function next() {
+    if (a == 2) {
+        a = 0
+    }
+    a++
+    console.log(a);
+    
+    switch (a) {
+        case 1:
+            let temp = image[0].src
+            image[0].src = image[1].src
+            image[1].src = temp
+            break;
+        case 2:
+            let temp1 = image[0].src
+            image[0].src = image[2].src
+            image[2].src = temp1
+            break;
+    }
+}
+
+let b=0
+function back() {
+    if (b == 3) {
+        b = 0
+    }
+    b++
+    switch (b) {
+        case 1:
+            let tempo = image[0].src
+            image[0].src = image[1].src
+            image[1].style=
+            image[1].src = tempo
+            break;
+        case 2:
+            let tempo1 = image[0].src
+            image[0].src = image[2].src
+            image[2].src = tempo1
+            break;
+       
+    }
+}
---
+++ PassStrength/cocept.js
@@ -0,0 +1,20 @@
+let input = document.querySelector("input")
+let output = document.querySelector("p")
+
+input.addEventListener('input', () => {
+    let outputText = input.value
+    output.style.color = " blueviolet"
+    input.style.border = "2px solid  blueviolet"
+    output.textContent = `Password is Weak`
+    if (outputText.includes("%") || outputText.includes("@") || outputText.includes("#") || outputText.includes("$") || outputText.includes("&")) {
+        output.style.color = "green"
+        input.style.border = "2px solid green"
+        output.textContent = `Password is strong`
+    }
+    if (outputText.includes("@#")) {
+        output.style.color = "red"
+        input.style.border = "2px solid red"
+        output.textContent = `Password is Super strong`
+    }
+}
+)
